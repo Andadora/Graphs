b530dc7fdac845c7c737feb958deb78cb1881f99
Andadora <j.dorobisz@wp.pl>
2019-04-26 15:50:56 +0200

    dodanie pełnj obsługi wyników do testów

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-import GraphMatrix as GM
-import GraphList as GL
-import GraphData as GD
+import GraphMatrix
+import GraphList
+import GraphData
+import time
 
-GD.graph_generate_file(10, 0.5, 0, 'graph.txt')
-file = open('graph.txt', 'r').read()
-graph = GL.GraphList(file)
-results = open('results.txt', 'w')
-
-results.write(str(graph.dijkstra()))
 
 representation_list = ['List', 'Matrix']
 vertices_list = [10, 50, 100, 500, 1000]
-density_list = [0.25, 0.50, 0.75, 1]
+density_list = [0.25, 0.50, 0.75, 1.00]
 
-# for vertices in vertices_list:
-#    for density in density_list:
-#        for representation in representation_list:
-#            for i in range(100):
+results = open('test.txt', 'a')
 
-# GD.graph_generate_file(vertices, density, 0, 'graph.txt')
-
+for representation in representation_list:
+    for vertices in vertices_list:
+        for density in density_list:
+            duration_100 = 0
+            for i in range(1):
+                GraphData.graph_generate_file(vertices, density, 0, 'graph.txt')
+                file = open('graph.txt', 'r').read()
+                if representation == 'List':
+                    graph = GraphList.GraphList(file)
+                    start = time.perf_counter_ns()
+                    graph.dijkstra()
+                    end = time.perf_counter_ns()
+                    duration = end - start
+                    duration_100 += duration
+                if representation == 'Matrix':
+                    graph = GraphMatrix.GraphMatrix(file)
+                    start = time.perf_counter_ns()
+                    graph.dijkstra()
+                    end = time.perf_counter_ns()
+                    duration = end - start
+                    duration_100 += duration
+            average_duration = duration_100/1
+            results.write('r: {:6s}, v: {:5d}, d: {:.2f}, t: {:11.0f} ns\n'.format(representation, vertices, density, average_duration))
+results.close()
